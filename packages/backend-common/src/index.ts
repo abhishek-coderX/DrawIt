75ab9acb027d27e81eaa3ace9ce87d1d50d2723a
Abhishek Pal <abhipal2330@gmail.com>
2026-06-16 17:49:05 +0530

    updated

--- packages/backend-common/src/index.ts
+++ packages/backend-common/src/index.ts
@@ -1,2 +1,7 @@
+const JWT_SECRET = process.env.JWT_SECRET;
 
-export const JWT_SECRET = process.env.JWT_SECRET || "123123";
+if (!JWT_SECRET) {
+  throw new Error("JWT_SECRET is not defined");
+}
+
+export { JWT_SECRET };
